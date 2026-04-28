4431b9d0f7c190a650e25d782d42047118e0242a
Krystal Visone <krystal.visone89@qmail.cuny.edu>
2026-04-27 23:44:27 -0400

    Save current project setup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-<p class='font-bold text-gray-900'>Hello</p>
+<p class="font-bold text-gray-900">Hello world</p>
+
